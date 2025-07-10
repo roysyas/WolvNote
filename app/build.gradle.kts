b1fb3712cbba8329b0d9e52c70525d5a46c9774a
roys-quadrant <roys@quadrant.io>
2025-07-10 21:45:43 +0700

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.roys.wolvnote"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
